16361874d15e3493a9d32b411859151d834ee3f3
w <w@xvk3.net>
2019-08-31 13:27:43 +0100

    upgraded ipf.sh so it doesn't use cat

--- ipf.sh
+++ ipf.sh
@@ -1,1 +1,1 @@
-cat /var/log/ufw.log | grep -Eo "SRC=([0-9]{1,3}[\.]){3}[0-9]{1,3}" | awk '{gsub("SRC=", "");print}' | sort | uniq -c | sort -k1,1nr -k2
+grep -Eo "SRC=([0-9]{1,3}[\.]){3}[0-9]{1,3}" /var/log/ufw.log | awk '{gsub("SRC=", "");print}' | sort | uniq -c | sort -k1,1nr -k2
