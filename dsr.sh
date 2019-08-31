2dde9e8d67787fd0aa1145e568e7032c7382d177
w <w@xvk3.net>
2019-08-31 11:05:31 +0100

    added dsr.sh script

---
+++ dsr.sh
@@ -0,0 +1,14 @@
+players="$(curl -s https://steamcommunity.com/app/570940 | grep -Po -m 1 "(?<=>).*(?= In-Game)" | sed 's/,//g')"
+echo "players= $players"
+echo $players >> ~/trd/dsr.log
+echo $players >> ~/stor/trd/dsr.log
+lines=$(wc -l < ~/trd/dsr.log)
+echo "lines= $lines"
+remove=$((lines-168))
+echo "remove= $remove"
+if [ $remove -ge 1 ]
+    then
+    echo "removing $remove lines"
+    echo "sed -i -e '1,${remove}d' ~/trd/dsr.log"
+    sed -i -e 1,${remove}d ~/trd/dsr.log
+fi
